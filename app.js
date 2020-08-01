3ea64c9b22070d567db47950b4c3e872b05a6859
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-01 13:30:13 -0400

    functional pushes to allEmployees array

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const allEmployees = []; 
 
-
-promptEmployee(); 
-
 // inquirer to gathers information about the development team members
 // creates objects for each team member 
 
@@ -79,27 +76,28 @@ function promptEmployee() {
         if (answers.addMore === true){ 
             promptEmployee(); 
         } else { 
-            return; 
+            console.log(allEmployees); 
+            createTempHTML(allEmployees); 
         }
     })
 };
 
-console.log(allEmployees); 
-render(allEmployees);
+function createTempHTML(allEmployees){ 
+    const tempHTML = render(allEmployees);  
+    fs.writeFile("team.html", tempHTML); 
+    console.log("successfully wrote to team.html");
+    
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+promptEmployee(); 
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+
+// You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+
+
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
